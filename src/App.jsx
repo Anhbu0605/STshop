2f46ba53000211d72d16bb544776fb1aa590917a
minhthuan1809 <thuan1809minh@gmail.com>
2025-03-21 09:30:21 +0700

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect } from "react";
 import RouterDom from "./router/RouterDom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 export default function App() {
+  useEffect(() => {
+    //chống tắt sever 10p call 1 lần
+    const keepServerAlive = () => {
+      const img = new Image();
+      img.src = "https://stshop.onrender.com/";
+    };
+    setInterval(() => {
+      keepServerAlive();
+    }, 600000);
+  }, []);
   return (
     <div>
       <RouterDom />
